8e1f0b5b287a455e10a8baa9b447106d6adffb51
joaquin <juaco1718@gmail.com>
2021-04-24 14:50:41 -0300

    initial commit

---
+++ hotel_scrapping/test/functional_test.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import unittest
+
+
+
+class StartServer(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_server(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('successfully', self.browser.title)
+        #self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
