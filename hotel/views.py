968faaa8730e1144db67d8fc7082dc56373c798b
joaquin <juaco1718@gmail.com>
2021-04-27 02:44:22 -0300

    heroku deploy

--- hotel/views.py
+++ hotel/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import requests
+import time
+from bs4 import BeautifulSoup
 
 
 # Create your views here.
 
 
+def get_rendered_html_data(url):
+    chrome_options = Options()
+    # chrome_options.headless=True
+    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+    driver.get(url)
+    time.sleep(5)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.quit()
+    return soup
+
+
 def get_html_data(url):
-    import requests
     USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
     LANGUAGE = "en-US,en;q=0.5"
     session = requests.Session()
@@ -13,61 +29,73 @@ def get_html_data(url):
     session.headers['Accept-Language'] = LANGUAGE
     session.headers['Content-Language'] = LANGUAGE
     html = session.get(url).text
-    from bs4 import BeautifulSoup
     soup = BeautifulSoup(html, 'html.parser')
-    print(soup.prettify())
     return soup
 
 
 def get_hotel_from_html_soup(url, soup):
+    hotel = {}
     # Nombre del hotel
-    nombre = soup.find(id="hp_hotel_name")
-    print(f'Nombre: {nombre.text}')
+    name = soup.find(id="hp_hotel_name")
+    hotel['name'] = name.text
 
     # Dirección
-    dire = soup.find("span", class_="hp_address_subtitle")
-    print(f'Dirección: {dire.text}')
+    address = soup.find("span", class_="hp_address_subtitle")
+    hotel['address'] = address.text
 
-
-# URL de las 5 principales fotos del hotel
+    # URL de las 5 principales fotos del hotel
     pictures = soup.find("span", class_="hp_address_subtitle")
     pictures_link = pictures.find_all_next("a", class_='bh-photo-grid-thumb')
+    hotel['pictures'] = []
     for pic in pictures_link:
-        print(f'FOTOS: {pic["href"]}')
+        hotel['pictures'].append(pic["href"])
 
-# Para cada tipo de habitación del hotel:
-    rooms_table = soup.find_all('table', class_='room-info')
+    # Para cada tipo de habitación del hotel:
+    rooms_table = soup.find_all('div', class_='room-info')
+    hotel['rooms'] = []
     for room in rooms_table:
-        room_link = pictures.find_all("a", class_='togglelink')
-# Nombre
-        nombre = room_link['data-room-name-en']
-        print(f'Cuarto con Nombre: {nombre}')
-        room_html = get_html_data(url+"#"+room_link)
-# Tamaño de la habitación
-        room_size = room_html.select('p[data-name-en="roomsize"]').next_sibling
-        print(f'    SIZE: {room_size.text}')
-# URL de las 5 principales fotos del tipo de habitación
-        room_pictures = room_html.find('div', class_='b_nha_hotel_small_images')
-        pictures_link = room_pictures.find_all_next('img')
+        dic_room = {}
+        room_link = room.find("a", class_='togglelink')
+
+        # Nombre
+        name = room_link['data-room-name-en']
+        dic_room['name']= name
+        room_html = get_rendered_html_data(url + room_link['href'].replace('RD', 'room_'))
+
+        # Tamaño de la habitación
+        room_size = room_html.find('p', attrs={'data-name-en': "roomsize"}).next_sibling.next_sibling
+        dic_room['size'] = room_size
+
+        # URL de las 5 principales fotos del tipo de habitación
+        room_pictures = room_html.find('div', class_='slick-track')
+        pictures_link = room_pictures.find_all_next('img', limit=5)
+        dic_room['pictures'] = []
         for pic in pictures_link:
-            print(f'FOTOS: {pic["src"]}')
-            print(f'    FOTOS: {pic["src"]}')
+            if 'src' in pic.attrs:
+                dic_room['pictures'].append(pic["src"])
+            elif 'data-lazy' in pic.attrs:
+                dic_room['pictures'].append(pic["data-lazy"])
+        hotel['rooms'].append(dic_room)
 
-# Listado completo de amenities y equipamiento de la habitación
 
+    # Listado completo de amenities y equipamiento de la habitación
+    # pending
 
-# Calificación del hotel y cantidad de reviews recibidos
-    calif = soup.find("div", class_="bui-review-score__badge")
-    print(f'Calificación del hotel: {calif.text}')
-    revi = soup.find("div", class_="bui-review-score__text")
-    print(f'Cantidad de reviews recibidos: {revi.text}')
+    # Calificación del hotel y cantidad de reviews recibidos
+    score = soup.find("div", class_="bui-review-score__badge")
+    hotel['score'] = score.text
+    review = soup.find("div", class_="bui-review-score__text")
+    hotel['review'] = review.text
+
+    return hotel
 
 
 def home_page(request):
+    hotel = None
     if 'url' in request.GET:
         url = request.GET.get('url')
         html_data = get_html_data(url)
-        get_hotel_from_html_soup(url, html_data)
+        hotel = get_hotel_from_html_soup(url, html_data)
+        print(hotel)
         # fetch the data from booking.com
-
-    return render(request, 'hotel/home.html')
+    return render(request, 'hotel/home.html', {'hotel':hotel})
