373990673a7a6141828a70940c016cfa5e0c3969
joaquin <juaco1718@gmail.com>
2021-04-27 03:17:58 -0300

    selenium fixes

--- hotel/views.py
+++ hotel/views.py
@@ -12,13 +12,13 @@ import os
 
 def get_rendered_html_data(url):
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
+   # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(url)
-    time.sleep(5)
+    time.sleep(15)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     driver.quit()
     return soup
