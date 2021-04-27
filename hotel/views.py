851a09dcdf7d63d03f32e1c1abb742ecc9136d1a
joaquin <juaco1718@gmail.com>
2021-04-27 13:31:06 -0300

    avoid using selenium again

--- hotel/views.py
+++ hotel/views.py
@@ -5,23 +5,26 @@ from selenium.webdriver.chrome.options import Options
 import requests
 import time
 from bs4 import BeautifulSoup
-import os
+
 
 # Create your views here.
 
 
 def get_rendered_html_data(url):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.set_page_load_timeout(100)
-    driver.get(url)
-    time.sleep(5)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
+    # import HTMLSession from requests_html
+    from requests_html import HTMLSession
+
+    # create an HTML Session object
+    session = HTMLSession()
+
+    # Use the object above to connect to needed webpage
+    resp = session.get(url)
+
+    # Run JavaScript code on webpage
+    rendered_response = resp.html.render(timeout=100)
+
+    soup = BeautifulSoup(
+               rendered_response.html, 'html.parser')
     return soup
 
 
